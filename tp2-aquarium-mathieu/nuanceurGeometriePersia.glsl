06ce0de1ebd5c34a6c3538f3cf614b6b7ad12997
ShomaliParty <37595534+ShomaliParty@users.noreply.github.com>
2020-10-10 19:05:06 -0400

    Update nuanceurGeometriePersia.glsl

--- tp2-aquarium-mathieu/nuanceurGeometriePersia.glsl
+++ tp2-aquarium-mathieu/nuanceurGeometriePersia.glsl
@@ -1,22 +1,22 @@
+#version 460
+
 layout(triangles) in;
-layout(triangle_strip, max vertices = 3) out;
+layout(triangle_strip, max_vertices = 3) out;
 
 
-void produitvectoriel(glm::vec3 vecA, glm::vec3 vecB, glm::vec3 resultat) 
-  
-{ 
-    resultat[0] = vecA[1] * vecB[2] - vecA[2] * vecB[1]; 
-    resultat[1] = vecA[2] * vecB[0] - vecA[0] * vecB[2]; 
-    resultat[2] = vecA[0] * vecB[1] - vecA[1] * vecB[0]; 
-} 
 
-void main(void) 
+
+
+
+void main() 
 {
-    glm::vec3 arete1 = gl_in[1].gl_Position - gl_in[0].gl_Position;
-    glm::vec3 arete2 = gl_in[2].gl_Position - gl_in[1].gl_Position;
-    glm::vec3 normale;
+    vec4 arete1 = gl_in[1].gl_Position - gl_in[0].gl_Position;
+    vec4 arete2 = gl_in[2].gl_Position - gl_in[1].gl_Position;
+    vec4 normale;
 
-    produitVectoriel(arete1, arete2, normale);
+    normale[0] = arete1[1] * arete2[2] - arete1[2] * arete2[1]; 
+    normale[1] = arete1[2] * arete2[0] - arete1[0] * arete2[2]; 
+    normale[2] = arete1[0] * arete2[1] - arete1[1] * arete2[0]; 
 
     EmitVertex();
 
