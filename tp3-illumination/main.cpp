77b5af16a11cf7c93b9d8c67735c54334e1cf98b
ShomaliParty <37595534+ShomaliParty@users.noreply.github.com>
2020-11-14 14:22:24 -0500

    Update main.cpp

--- tp3-illumination/main.cpp
+++ tp3-illumination/main.cpp
@@ -519,8 +519,6 @@ void afficherModele()
             if (Etat::utiliseTess)
             {
                 // partie 3: afficher le cube avec des GL_PATCHES
-                /*GLint nconnect = 40;
-                glDrawElements(GL_PATCHES, nconnect, GL_UNSIGNED_INT, 0);*/
                 glDrawArrays(GL_PATCHES, 0, 4);
                 glDrawArrays(GL_PATCHES, 4, 4);
                 glDrawArrays(GL_PATCHES, 8, 4);
